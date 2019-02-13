aef35e287b8a7cac90b56508276aab21b7f431f5
Jonathan Cancio <jonahcancio@gmail.com>
2019-02-13 08:50:12 +0800

    Initial commit: Vue now shows up in Django
    The basic boilerplate Vue page can now be seen in Django localhost:8000. Just had to specify publicPath and static url

--- diskover_plusplus/settings.py
+++ diskover_plusplus/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'diskover_plusplus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'dist'),
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 STATIC_URL = '/static/'
+
