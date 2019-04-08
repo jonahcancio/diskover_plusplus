f066290c2a3c2c78cc8d43acef02c811db49481b
Mikhael Dionio <mikhaeldionio@gmail.com>
2019-04-08 10:02:28 +0800

    add initial form layout

---
+++ src/pages/CategoryFormPage.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container grid-list-lg class="grey lighten-4">
+    <v-card>
+        <v-card-title class="primary">
+            <h1 class="white--text">Edit Category</h1>
+        </v-card-title>
+        <v-container>
+            <v-layout column>
+            <v-flex xs12>
+                <label>Name</label>
+                <v-text-field
+                placeholder="Name of the category"
+                color="black"
+                v-model="name"
+                :readonly="isReadOnly"
+                :error="isReadOnly"
+                />
+            </v-flex>
+            <v-flex xs12>
+                <label>URL</label>
+                <v-text-field
+                    v-model="url"
+                    color="black"
+                    auto-grow
+                    placeholder="Url to be displayed for this category"
+                    :readonly="isReadOnly"
+                    :error="isReadOnly"
+                />
+            </v-flex>
+            <v-flex xs12>
+                <label>Route Color</label>
+                <div>Pick a color:<input type="color" @change="onColorSelected"></div>
+            </v-flex>
+            <v-flex xs12>
+                <label>Image</label>
+                <div><input type="file" @change="onImageFileSelected"></div>
+            </v-flex>
+            <v-flex xs12>
+                <label>Marker</label>
+                <div><input type="file" @change="onImageFileSelected"></div>
+            </v-flex>
+            <v-flex xs12>
+                <label>Route Marker</label>
+                <div><input type="file" @change="onImageFileSelected"></div>
+            </v-flex>
+        </v-layout>
+        <v-card-actions>
+            <v-btn class="primary white--text">Submit</v-btn>
+        </v-card-actions>
+        </v-container>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            imageFile: null,
+            color: null
+        }
+    },
+    methods:{
+        onImageFileSelected(event){
+            this.imageFile = event.target.files[0]
+        },
+        onColorSelected(event){
+            this.color = event.target.value
+            console.log(this.color)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    label {
+    font-weight: bold !important;
+    font-size: 16px !important;
+    }
+</style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router'
 // import pages
 import HomePage from "@/pages/HomePage"
 // import FormPage from "@/pages/FormPage"
+import CategoryFormPage from "@/pages/CategoryFormPage"
 import MapPage from "@/pages/MapPage"
 import ResultSide from "@/pages/MapPage/ResultSide"
 import DetailSide from "@/pages/MapPage/DetailSide"
@@ -52,6 +53,12 @@ const router = new VueRouter({
       path:"*",
       name: "404",
       component: Error404Page
+    },
+    //category form page
+    {
+      path:"/categoryform",
+      name: "categoryform",
+      component: CategoryFormPage,
     }
 	]
 })
